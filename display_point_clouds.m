ae22ca8149936bae493423d89c1934e2be276b41
Paige Yeung <paige.yeung@gmail.com>
2022-06-15 11:07:09 -0700

    Add new averaged normals

--- display_point_clouds.m
+++ display_point_clouds.m
@@ -7,6 +7,8 @@ y = T.("y (m)")
 z = T.("z (m)")
 intens = T.intensity
 xyz = [x, y, z];
+
+writematrix(xyz,'coordinates.csv')
 pc = pointCloud(xyz, 'Intensity', intens);
 
 lower = min([pc.XLimits pc.YLimits]);
@@ -16,15 +18,15 @@ xlimits = [lower upper];
 ylimits = [lower upper];
 zlimits = pc.ZLimits;
 
-player = pcplayer(xlimits,ylimits,zlimits);
-
-xlabel(player.Axes,'X (m)');
-ylabel(player.Axes,'Y (m)');
-zlabel(player.Axes,'Z (m)');
-
-for i = 1:360      
-    view(player,pc);     
-end
+% player = pcplayer(xlimits,ylimits,zlimits);
+% 
+% xlabel(player.Axes,'X (m)');
+% ylabel(player.Axes,'Y (m)');
+% zlabel(player.Axes,'Z (m)');
+% 
+% for i = 1:360      
+%     view(player,pc);     
+% end
 
 normals = pcnormals(pc);
 x1 = pc.Location(1:100:end,1);
@@ -34,6 +36,8 @@ u = normals(1:100:end,1);
 v = normals(1:100:end,2);
 w = normals(1:100:end,3);
 
+writematrix(normals,'normals.csv');
+
 figure
 pcshow(pc)
 title('Estimated Normals of Point Cloud')
@@ -41,21 +45,20 @@ hold on
 quiver3(x1,y1,z1,u,v,w);
 hold off
 
-index = 2;
-incx = 10;
-incy = 10;
+%% Divide point cloud into cells
+
+index = 1;
+incx = 50;
+incy = 50;
 incz = 5;
 
-%r = zeros(350/inc,175/inc,20/inc);
+r = zeros(size(x, 1),1);
 
-
-r = ones(size(x, 1),1);
-
-for i = min(pc.XLimits):incx:max(pc.XLimits)
-    for j = min(pc.YLimits):incy:max(pc.YLimits)
-        for k = min(pc.ZLimits):incz:max(pc.ZLimits)
+for i = min(pc.XLimits)-1:incx:max(pc.XLimits)
+    for j = min(pc.YLimits)-1:incy:max(pc.YLimits)
+        for k = min(pc.ZLimits)-1:incz:max(pc.ZLimits)
             indices = x>i & x<=i+incx & y>j & y<=j+incy & z>k & z<=k+incz;
-            size(indices(indices==1))
+            %size(indices(indices==1))
             r(indices) = index;
             index = index + 1;
         end
@@ -63,9 +66,79 @@ for i = min(pc.XLimits):incx:max(pc.XLimits)
 end
 
 writematrix(r,'indexes.csv');
+cmap = jet(index);
 
-%cmap = jet(20000);
-%figure(), scatter3(x,y,z,10,cmap(r,:),'filled')
+%% Create new point cloud
+newPoints = zeros(index,3);
+
+for i = 1:index
+    if size(x(r==i),1) > 0
+        newPoints(i,1) = mean(x(r==i));
+        newPoints(i,2) = mean(y(r==i));
+        newPoints(i,3) = mean(z(r==i));
+    end
+end
+figure(), scatter3(x,y,z,10,cmap(r,:),'filled')
+
+newPc = pointCloud(newPoints);
+
+lower = min([newPc.XLimits newPc.YLimits]);
+upper = max([newPc.XLimits newPc.YLimits]);
+  
+xlimits = [lower upper];
+ylimits = [lower upper];
+zlimits = newPc.ZLimits;
+
+player = pcplayer(xlimits,ylimits,zlimits);
+
+xlabel(player.Axes,'X (m)');
+ylabel(player.Axes,'Y (m)');
+zlabel(player.Axes,'Z (m)');
+
+for i = 1:360      
+    view(player,newPc);     
+end
+
+newNormals = pcnormals(newPc);
+x1 = newPc.Location(1:100:end,1);
+y1 = newPc.Location(1:100:end,2);
+z1 = newPc.Location(1:100:end,3);
+u = newNormals(1:100:end,1);
+v = newNormals(1:100:end,2);
+w = newNormals(1:100:end,3);
+
+figure
+pcshow(newPc)
+title('Estimated Normals of New Point Cloud')
+hold on
+quiver3(x1,y1,z1,u,v,w);
+hold off
 
 %dividednormals = horzcat(normals,r);
 
+%% New normals
+newNormals = zeros(index, 3);
+
+for i = 1:index
+    if size(x(r==i),1) > 0
+        newNormals(i,1) = 100*mean(normals(r==i,1));
+        newNormals(i,2) = 100*mean(normals(r==i,2));
+        newNormals(i,3) = 100*mean(normals(r==i,3));
+    end
+end
+
+x1 = newPc.Location(1:1:end,1);
+y1 = newPc.Location(1:1:end,2);
+z1 = newPc.Location(1:1:end,3);
+u = newNormals(1:1:end,1);
+v = newNormals(1:1:end,2);
+w = newNormals(1:1:end,3);
+
+writematrix(newNormals, 'newNormals.csv')
+
+figure
+pcshow(newPc)
+title('Estimated Normals of New Point Cloud (2)')
+hold on
+quiver3(x1,y1,z1,u,v,w);
+hold off
